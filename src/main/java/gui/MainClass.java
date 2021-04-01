be9ad262b370c1350e4439a34ce44668318067c0
EngRobot33 <hmdkhsrvee@gmail.com>
2021-04-01 22:09:31 +0430

    first commit

---
+++ src/main/java/gui/MainClass.java
@@ -0,0 +1,12 @@
+package main.java.gui;
+
+public class MainClass {
+    
+    public static void main(String[] args){
+        
+        mainFrame mainframe = new mainFrame();
+        mainframe.showPanel();
+        
+    }
+    
+}
